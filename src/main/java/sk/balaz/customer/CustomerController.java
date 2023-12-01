68325d67b0f8be60295de989e7a381bf71ddec61
Tomas Balaz <balaztomas0@gmail.com>
2023-12-01 13:48:17 +0100

    Example of the usage wiring classes without Dependency injection

--- src/main/java/sk/balaz/customer/CustomerController.java
+++ src/main/java/sk/balaz/customer/CustomerController.java
@@ -2,13 +2,19 @@ package sk.balaz.customer;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sk.balaz.Application;
 
 @RestController
 public class CustomerController {
 
+   // private final CustomerService customerService = new CustomerService();
+    private CustomerService customerService;
+
+    public CustomerController() {
+        this.customerService = new CustomerService();
+    }
+
     @GetMapping
     public Customer getCustomer() {
-        return new Customer(1l, "James Bond");
+        return customerService.getCustomer();
     }
 }
--- src/main/java/sk/balaz/customer/CustomerService.java
+++ src/main/java/sk/balaz/customer/CustomerService.java
@@ -1,4 +1,8 @@
 package sk.balaz.customer;
 
 public class CustomerService {
+
+    public Customer getCustomer() {
+        return new Customer(1l, "James Bond");
+    }
 }
