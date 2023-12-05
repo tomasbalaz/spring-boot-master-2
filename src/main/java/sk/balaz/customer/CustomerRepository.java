bf99c7c00c7da19e29ee07ec26a47bddb77dcbb0
Tomas Balaz <balaztomas0@gmail.com>
2023-12-05 14:09:56 +0100

    Add @Primary in CustomerRepository

--- src/main/java/sk/balaz/customer/CustomerRepository.java
+++ src/main/java/sk/balaz/customer/CustomerRepository.java
@@ -1,11 +1,13 @@
 package sk.balaz.customer;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collections;
 import java.util.List;
 
 @Repository
+@Primary
 public class CustomerRepository implements CustomerRepo {
     @Override
     public List<Customer> getCustomers() {
