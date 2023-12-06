d69c6cd3946b03b5a822bfcc2b941e02e95659c2
Tomas Balaz <balaztomas0@gmail.com>
2023-12-06 14:51:50 +0100

    Introduction to Jackson Fasterxml annotations in Spring boot

--- src/main/java/sk/balaz/customer/Customer.java
+++ src/main/java/sk/balaz/customer/Customer.java
@@ -1,6 +1,14 @@
 package sk.balaz.customer;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+// Jackson Fasterxml annotations
+// https://www.geeksforgeeks.org/jackson-annotations-for-java-application/
 public record Customer(
+        @JsonProperty("customer_id")
         long id,
-        String name) {
+        String name,
+        @JsonIgnore
+        String password) {
 }
--- src/main/java/sk/balaz/customer/CustomerFakeRepo.java
+++ src/main/java/sk/balaz/customer/CustomerFakeRepo.java
@@ -6,8 +6,8 @@ public class CustomerFakeRepo implements CustomerRepo {
     @Override
     public List<Customer> getCustomers() {
         return List.of(
-                new Customer(1l, "James Bond"),
-                new Customer(2l, "Tomas Tomas")
+                new Customer(1l, "James Bond","1234"),
+                new Customer(2l, "Tomas Tomas", "1234")
         );
     }
 }
